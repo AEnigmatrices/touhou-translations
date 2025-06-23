6fb102514204b7fc51d8562efa75590954fee751
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-06-24 00:07:35 +0700

    feat: Replace gallery page with placeholder for now

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,19 +1,10 @@
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import { useRoutes } from 'react-router-dom';
-import postsData from '../data/posts.json';
-import type { Post } from '../types/data';
 import { createRoutes } from './routes';
 
-const typedPosts = postsData as Post[];
-
 const AppRoutes = () => {
-    const [currentPostIndex, setCurrentPostIndex] = useState<number>(0);
-    const post = typedPosts[currentPostIndex];
 
-    const routes = useMemo(() =>
-        createRoutes(post, currentPostIndex, setCurrentPostIndex, typedPosts.length),
-        [post, currentPostIndex, setCurrentPostIndex]
-    );
+    const routes = useMemo(() => createRoutes(), []);
 
     return useRoutes(routes);
 };
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,17 +1,14 @@
-import type { RouteObject } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Item from "../pages/Item/Item";
 import Placeholder from "../components/Placeholder";
-import type { Post } from "../types/data";
-import Gallery from "../pages/Gallery/Gallery";
 
 
 
-export const createRoutes = (post: Post, currentPostIndex: number, setCurrentPostIndex: React.Dispatch<React.SetStateAction<number>>, totalPosts: number): RouteObject[] => [
+export const createRoutes = () => [
     { path: "/", element: <Home title="Home" /> },
     { path: "/search", element: <Placeholder title="Search" /> },
     { path: "/characters", element: <Placeholder title="Characters" /> },
     { path: "/artists", element: <Placeholder title="Artists" /> },
-    { path: "/gallery", element: (<Gallery post={post} currentPostIndex={currentPostIndex} setCurrentPostIndex={setCurrentPostIndex} totalPosts={totalPosts} />) },
+    { path: "/gallery", element: <Placeholder title="Gallery" /> },
     { path: "/post/:id", element: <Item /> }
 ];
