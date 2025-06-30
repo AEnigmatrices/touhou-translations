c734c792a5eed67e3d205665030e85c82a211e30
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-06-30 23:05:37 +0700

    refactor: Simplify replaceXWithNitter

--- app/src/utils/urlUtils.ts
+++ app/src/utils/urlUtils.ts
@@ -1,12 +1,8 @@
 export const replaceXWithNitter = (originalUrl: string) => {
     try {
         const url = new URL(originalUrl);
-        if (url.hostname === 'x.com' || url.hostname.endsWith('.x.com')) {
-            url.hostname = url.hostname.replace(/x\.com$/i, 'nitter.net');
-            return url.toString();
-        }
-        return null;
-    } catch (e) {
+        return url.hostname === 'x.com' ? (url.hostname = 'nitter.net', url.toString()) : null;
+    } catch {
         return null;
     }
-}
+};
