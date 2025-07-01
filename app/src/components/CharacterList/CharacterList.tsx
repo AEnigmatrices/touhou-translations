fbfa4715f0aa1d8e167f0887046ed5e8af64bdcb
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-07-01 08:40:21 +0700

    feat: Link to Gallery

--- app/src/components/CharacterList/CharacterList.tsx
+++ app/src/components/CharacterList/CharacterList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { getCharacterImages } from "../../utils/characterImages";
 import { getGradient } from "../../utils/gradientUtils";
 import { useGetCharacters } from "../../context/PostsContext";
@@ -17,20 +18,23 @@ const CharacterList: React.FC = () => {
                 const gradient = getGradient(hue, 25, 87);
                 const gradientPlaceholder = getGradient(hue, 25, 73);
                 const imageUrl = getCharacterImages(character.id);
+                const toUrl = `/gallery?character=${character.id}`;
                 return (
-                    <li key={character.id} className="character-list__item character-list__item--mobile-row" tabIndex={0} aria-label={`Character: ${character.name}`}  >
-                        <div className="character-list__image-wrapper character-list__image-wrapper--mobile" style={{ background: gradient }} >
-                            {imageUrl ? (
-                                <img src={imageUrl} alt={character.name} className="character-list__image" loading="lazy" />
-                            ) : (
-                                <div className="character-list__image-placeholder" aria-hidden="true" style={{ background: gradientPlaceholder }} />
-                            )}
-                        </div>
-                        <div className="character-list__info character-list__info--mobile">
-                            <span className="character-list__name character-list__name--mobile-left">
-                                {character.name}
-                            </span>
-                        </div>
+                    <li key={character.id} className="character-list__item" aria-label={`Character: ${character.name}`} >
+                        <Link to={toUrl} className="character-list__link">
+                            <div className="character-list__image-wrapper" style={{ background: gradient }}>
+                                {imageUrl ? (
+                                    <img src={imageUrl} alt={character.name} className="character-list__image" loading="lazy" />
+                                ) : (
+                                    <div className="character-list__image-placeholder" aria-hidden="true" style={{ background: gradientPlaceholder }} />
+                                )}
+                            </div>
+                            <div className="character-list__info">
+                                <span className="character-list__name">
+                                    {character.name}
+                                </span>
+                            </div>
+                        </Link>
                     </li>
                 );
             })}
