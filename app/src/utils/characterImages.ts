370ddc6654500b37ee8d7175435c1f1464805ef1
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-07-06 13:07:51 +0700

    feat: Placeholder images

--- app/src/utils/characterImages.ts
+++ app/src/utils/characterImages.ts
@@ -3,7 +3,15 @@ const characterImages: Record<string, string> = import.meta.glob(
     { eager: true, query: "?url", import: "default" }
 );
 
+const placeholderImages: Record<string, string> = import.meta.glob(
+    "../icons/placeholders/*.webp",
+    { eager: true, query: "?url", import: "default" }
+);
+
 export const getCharacterImages = (id: string): string | null => {
-    const relativePath = `../icons/characters/${id}.webp`;
-    return characterImages[relativePath] ?? null;
+    const path = `../icons/characters/${id}.webp`;
+    if (path in characterImages) return characterImages[path];
+
+    const placeholders = Object.values(placeholderImages);
+    return placeholders.length ? placeholders[Math.floor(Math.random() * placeholders.length)] : null;
 };
