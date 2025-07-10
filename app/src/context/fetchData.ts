0fe1e7180d73f181c992e8aef3e35273339ddf1d
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-07-10 22:52:09 +0700

    refactor: Keep postsIndex in separate JSON

--- app/src/context/fetchData.ts
+++ app/src/context/fetchData.ts
@@ -1,3 +1,4 @@
+import fileList from "./postsIndex.json";
 import type { Post, Artist, Character } from "../types/data";
 
 const baseUrl = import.meta.env.VITE_DATA_BASE_URL;
@@ -11,7 +12,6 @@ const fetchJson = async <T>(path: string): Promise<T> => {
 
 
 export const fetchPosts = async (): Promise<Post[]> => {
-    const fileList = ["posts-2024.json"];
     const postsArrays = await Promise.all(
         fileList.map(filename => fetchJson<Post[]>(`posts/${filename}`))
     );
