7e0fd8144f71f9f507ca6fd8d6683a65ea1731cc
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-07-17 19:50:32 +0700

    refactor: use SortOrder in utils too

--- app/src/pages/ArtistPage/ArtistPage.utils.ts
+++ app/src/pages/ArtistPage/ArtistPage.utils.ts
@@ -1,11 +1,11 @@
-import type { Artist } from "../../types/data";
+import type { Artist, SortOrder } from "../../types/data";
 
 export const searchArtists = (artists: Artist[], query: string): Artist[] =>
     artists.filter(({ id, name }) =>
         [id, name].some(field => field.toLowerCase().includes(query.toLowerCase()))
     );
 
-export const sortArtists = (artists: Artist[], order: "asc" | "desc" | "none"): Artist[] =>
+export const sortArtists = (artists: Artist[], order: SortOrder): Artist[] =>
     order === "none"
         ? artists
         : [...artists].sort((a, b) => order === "asc"
--- app/src/pages/CharacterPage/CharacterPage.utils.ts
+++ app/src/pages/CharacterPage/CharacterPage.utils.ts
@@ -1,11 +1,11 @@
-import type { Character } from "../../types/data";
+import type { Character, SortOrder } from "../../types/data";
 
 export const searchCharacters = (characters: Character[], query: string): Character[] =>
     characters.filter(({ id, name }) =>
         [id, name].some(field => field.toLowerCase().includes(query.toLowerCase()))
     );
 
-export const sortArtworkCounts = (characters: Character[], order: "asc" | "desc" | "none"): Character[] =>
+export const sortArtworkCounts = (characters: Character[], order: SortOrder): Character[] =>
     order === "none"
         ? characters
         : [...characters].sort((a, b) => order === "asc"
