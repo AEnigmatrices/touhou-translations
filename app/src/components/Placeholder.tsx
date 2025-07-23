8952a7ebbc66460d49026f45dcf2e3d43578340a
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-07-23 22:44:13 +0700

    refactor: Remove Placeholder page component

--- app/src/components/Placeholder.tsx
+++
@@ -1,9 +0,0 @@
-interface PlaceholderProps {
-    title: string;
-}
-
-const Placeholder = ({ title }: PlaceholderProps) => {
-    return <h2>{title} Page (Coming Soon)</h2>;
-};
-
-export default Placeholder;
