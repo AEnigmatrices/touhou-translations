61c8d385fe06c3c71818eac53f6a9b8eb4ea4303
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-07-23 22:40:34 +0700

    refactor: Move QueryClientProvider and ThemeProvider up

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,29 +1,21 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import Routes from './Routes';
 import PostsProvider from './context/PostsProvider';
 import ErrorBoundary from './context/ErrorBoundary';
 
-const theme = createTheme({
-    typography: { fontFamily: '"Noto Sans JP", "Roboto", "Helvetica Neue", Helvetica, Arial, sans-serif' }
-});
-
 const LoadingFallback = () => <div>Loading...</div>;
 
 const App: React.FC = () => (
-    <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Router basename="/touhou-translations">
-            <ErrorBoundary>
-                <PostsProvider>
-                    <Suspense fallback={<LoadingFallback />}>
-                        <Routes />
-                    </Suspense>
-                </PostsProvider>
-            </ErrorBoundary>
-        </Router>
-    </ThemeProvider>
+    <Router basename="/touhou-translations">
+        <ErrorBoundary>
+            <PostsProvider>
+                <Suspense fallback={<LoadingFallback />}>
+                    <Routes />
+                </Suspense>
+            </PostsProvider>
+        </ErrorBoundary>
+    </Router>
 );
 
 export default App;
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,15 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
 import './index.css';
 import App from './App.tsx';
 
 const queryClient = new QueryClient();
 
+const theme = createTheme({
+    typography: { fontFamily: '"Noto Sans JP", "Roboto", "Helvetica Neue", Helvetica, Arial, sans-serif' }
+});
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <App />
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <App />
+            </ThemeProvider>
         </QueryClientProvider>
     </StrictMode>
 );
