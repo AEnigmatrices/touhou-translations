6f253ea6ee64094fb238110c84d5a294a61f7250
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-07-27 00:50:58 +0700

    chore: Correct URLs

--- src/components/Navbar/Navbar.utils.ts
+++ src/components/Navbar/Navbar.utils.ts
@@ -17,15 +17,15 @@ export const ElevationScroll: React.FC<ElevationScrollProps> = (props) => {
 
 export const navLinks = [
     { label: 'Home', to: `${BASE_PATH}/` },
-    { label: 'Characters', to: `${BASE_PATH}/characters` },
-    { label: 'Artists', to: `${BASE_PATH}/artists` },
-    { label: 'Gallery', to: `${BASE_PATH}/gallery` },
-    ...((isDev) ? [{ label: 'Admin', to: `${BASE_PATH}/admin` }] : []),
+    { label: 'Characters', to: `${BASE_PATH}/characters/` },
+    { label: 'Artists', to: `${BASE_PATH}/artists/` },
+    { label: 'Gallery', to: `${BASE_PATH}/gallery/` },
+    ...((isDev) ? [{ label: 'Admin', to: `${BASE_PATH}/admin/` }] : []),
 ];
 
 export const getRandomPostPath = (posts: { reddit: string }[]): string => {
     if (!posts || posts.length === 0) return `${BASE_PATH}/`;
     const randomPost = posts[Math.floor(Math.random() * posts.length)];
     const redditId = extractRedditId(randomPost.reddit);
-    return redditId ? `${BASE_PATH}/posts/${redditId}` : `${BASE_PATH}/`;
+    return redditId ? `${BASE_PATH}/posts/${redditId}/` : `${BASE_PATH}/`;
 }
