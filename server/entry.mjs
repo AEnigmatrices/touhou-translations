3f04f9097f1bbbed61533466348b172774c86f1c
AEnigmatrices <AEnigmatrices@users.noreply.github.com>
2025-07-28 11:59:01 +0000

    deploy: d300f6a836416f79b8ec82e61174220c4ee6f6d9

--- server/entry.mjs
+++ server/entry.mjs
@@ -1,42 +1,42 @@
 import{setGlobalContext_buildEntry as r}from"vike/__internal";/*! virtual:vike:entry:server [vike:pluginModuleBanner] */const t={},a={},s={},e={},o=[],l={},n=[{pageId:"/src/pages/admin",isErrorPage:void 0,routeFilesystem:{routeString:"/admin",definedAtLocation:"/src/pages/admin/"},loadConfigLazy:()=>({moduleId:"virtual:vike:pageConfigLazy:server:/src/pages/admin",moduleExports:import("./entries/src_pages_admin.mjs")}),configValuesSerialized:{isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},clientRouting:{type:"standard",definedAtData:{filePathToShowToUser:"/src/renderer/+config.ts",fileExportPathToShowToUser:["default","clientRouting"]},valueSerialized:{type:"js-serialized",value:!0}}}},{pageId:"/src/pages/artists",isErrorPage:void 0,routeFilesystem:{routeString:"/artists",definedAtLocation:"/src/pages/artists/"},loadConfigLazy:()=>({moduleId:"virtual:vike:pageConfigLazy:server:/src/pages/artists",moduleExports:import("./entries/src_pages_artists.mjs")}),configValuesSerialized:{isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},clientRouting:{type:"standard",definedAtData:{filePathToShowToUser:"/src/renderer/+config.ts",fileExportPathToShowToUser:["default","clientRouting"]},valueSerialized:{type:"js-serialized",value:!0}}}},{pageId:"/src/pages/characters",isErrorPage:void 0,routeFilesystem:{routeString:"/characters",definedAtLocation:"/src/pages/characters/"},loadConfigLazy:()=>({moduleId:"virtual:vike:pageConfigLazy:server:/src/pages/characters",moduleExports:import("./entries/src_pages_characters.mjs")}),configValuesSerialized:{isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},clientRouting:{type:"standard",definedAtData:{filePathToShowToUser:"/src/renderer/+config.ts",fileExportPathToShowToUser:["default","clientRouting"]},valueSerialized:{type:"js-serialized",value:!0}}}},{pageId:"/src/pages/gallery",isErrorPage:void 0,routeFilesystem:{routeString:"/gallery",definedAtLocation:"/src/pages/gallery/"},loadConfigLazy:()=>({moduleId:"virtual:vike:pageConfigLazy:server:/src/pages/gallery",moduleExports:import("./entries/src_pages_gallery.mjs")}),configValuesSerialized:{isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},clientRouting:{type:"standard",definedAtData:{filePathToShowToUser:"/src/renderer/+config.ts",fileExportPathToShowToUser:["default","clientRouting"]},valueSerialized:{type:"js-serialized",value:!0}}}},{pageId:"/src/pages/index",isErrorPage:void 0,routeFilesystem:{routeString:"/",definedAtLocation:"/src/pages/index/"},loadConfigLazy:()=>({moduleId:"virtual:vike:pageConfigLazy:server:/src/pages/index",moduleExports:import("./entries/src_pages_index.mjs")}),configValuesSerialized:{isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},clientRouting:{type:"standard",definedAtData:{filePathToShowToUser:"/src/renderer/+config.ts",fileExportPathToShowToUser:["default","clientRouting"]},valueSerialized:{type:"js-serialized",value:!0}}}},{pageId:"/src/pages/posts/@id",isErrorPage:void 0,routeFilesystem:{routeString:"/posts/@id",definedAtLocation:"/src/pages/posts/@id/"},loadConfigLazy:()=>({moduleId:"virtual:vike:pageConfigLazy:server:/src/pages/posts/@id",moduleExports:import("./entries/src_pages_posts_-id.mjs")}),configValuesSerialized:{isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},clientRouting:{type:"standard",definedAtData:{filePathToShowToUser:"/src/renderer/+config.ts",fileExportPathToShowToUser:["default","clientRouting"]},valueSerialized:{type:"js-serialized",value:!0}}}}],d={configValuesSerialized:{}},u=Object.assign({}),g={...u};t[".page"]=g;const p=Object.assign({}),c={...p};e[".page"]=c;const f=Object.assign({}),m={...f};t[".page.server"]=m;const y=Object.assign({}),v={...y};e[".page.server"]=v;const z=Object.assign({}),S={...z};a[".page.route"]=S;const E=Object.assign({}),h={...E};e[".page.client"]=h;const L=Object.freeze(Object.defineProperty({__proto__:null,neverLoaded:l,pageConfigGlobalSerialized:d,pageConfigsSerialized:n,pageFilesEager:a,pageFilesExportNamesEager:e,pageFilesExportNamesLazy:s,pageFilesLazy:t,pageFilesList:o},Symbol.toStringTag,{value:"Module"}));/*! virtual:@brillout/vite-plugin-server-entry:serverEntry [vike:pluginModuleBanner] */{const i={
-  "_ListPage.BuWjDXyZ.js": {
-    "file": "assets/js/ListPage.BuWjDXyZ.js",
+  "_ListPage.C3L88Wxa.js": {
+    "file": "assets/js/ListPage.C3L88Wxa.js",
     "name": "ListPage",
     "imports": [
-      "__onRenderClient.vVKCzuIK.js",
-      "_ProfileItem.BNvXYd52.js",
-      "_useFormControl.CvUq2NXJ.js",
-      "_TextField.D97Msb7p.js"
+      "__onRenderClient.CrOezyuq.js",
+      "_ProfileItem.BmjKx306.js",
+      "_useFormControl.EJpFiZXV.js",
+      "_TextField.DH3ceIwm.js"
     ]
   },
-  "_ProfileItem.BNvXYd52.js": {
-    "file": "assets/js/ProfileItem.BNvXYd52.js",
+  "_ProfileItem.BmjKx306.js": {
+    "file": "assets/js/ProfileItem.BmjKx306.js",
     "name": "ProfileItem",
     "imports": [
-      "__onRenderClient.vVKCzuIK.js",
-      "_renderPageClientSide.D8e623kA.js"
+      "__onRenderClient.CrOezyuq.js",
+      "_renderPageClientSide.DUFOS9QK.js"
     ]
   },
-  "_TextField.D97Msb7p.js": {
-    "file": "assets/js/TextField.D97Msb7p.js",
+  "_TextField.DH3ceIwm.js": {
+    "file": "assets/js/TextField.DH3ceIwm.js",
     "name": "TextField",
     "imports": [
-      "__onRenderClient.vVKCzuIK.js",
-      "_useFormControl.CvUq2NXJ.js"
+      "__onRenderClient.CrOezyuq.js",
+      "_useFormControl.EJpFiZXV.js"
     ]
   },
-  "__onRenderClient.vVKCzuIK.js": {
-    "file": "assets/js/_onRenderClient.vVKCzuIK.js",
+  "__onRenderClient.CrOezyuq.js": {
+    "file": "assets/js/_onRenderClient.CrOezyuq.js",
     "name": "_onRenderClient",
     "imports": [
-      "_renderPageClientSide.D8e623kA.js"
+      "_renderPageClientSide.DUFOS9QK.js"
     ],
     "css": [
       "assets/static/src_renderer_index-54ec5530.BBfZVkUH.css"
     ]
   },
-  "_renderPageClientSide.D8e623kA.js": {
-    "file": "assets/js/renderPageClientSide.D8e623kA.js",
+  "_renderPageClientSide.DUFOS9QK.js": {
+    "file": "assets/js/renderPageClientSide.DUFOS9QK.js",
     "name": "renderPageClientSide",
     "dynamicImports": [
       "virtual:vike:pageConfigLazy:client:/src/pages/admin",
@@ -51,114 +51,114 @@ import{setGlobalContext_buildEntry as r}from"vike/__internal";/*! virtual:vike:e
     "file": "assets/static/src_renderer_index-54ec5530.BBfZVkUH.css",
     "src": "_src_renderer_index-54ec5530.BBfZVkUH.css"
   },
-  "_useFormControl.CvUq2NXJ.js": {
-    "file": "assets/js/useFormControl.CvUq2NXJ.js",
+  "_useFormControl.EJpFiZXV.js": {
+    "file": "assets/js/useFormControl.EJpFiZXV.js",
     "name": "useFormControl",
     "imports": [
-      "__onRenderClient.vVKCzuIK.js"
+      "__onRenderClient.CrOezyuq.js"
     ]
   },
   "node_modules/vike/dist/esm/client/runtime-client-routing/entry.js": {
-    "file": "assets/entries/entry-client-routing.S_ktFtf-.js",
+    "file": "assets/entries/entry-client-routing.BFmqqXWz.js",
     "name": "entries/entry-client-routing",
     "src": "node_modules/vike/dist/esm/client/runtime-client-routing/entry.js",
     "isEntry": true,
     "imports": [
-      "_renderPageClientSide.D8e623kA.js"
+      "_renderPageClientSide.DUFOS9QK.js"
     ]
   },
   "virtual:vike:pageConfigLazy:client:/src/pages/admin": {
-    "file": "assets/entries/src_pages_admin.BAB2scWZ.js",
+    "file": "assets/entries/src_pages_admin.BB5f1n1g.js",
     "name": "entries/src/pages/admin",
     "src": "virtual:vike:pageConfigLazy:client:/src/pages/admin",
     "isEntry": true,
     "isDynamicEntry": true,
     "imports": [
-      "__onRenderClient.vVKCzuIK.js",
-      "_TextField.D97Msb7p.js",
-      "_useFormControl.CvUq2NXJ.js",
-      "_renderPageClientSide.D8e623kA.js"
+      "__onRenderClient.CrOezyuq.js",
+      "_TextField.DH3ceIwm.js",
+      "_useFormControl.EJpFiZXV.js",
+      "_renderPageClientSide.DUFOS9QK.js"
     ],
     "css": [
       "assets/static/src_renderer_index-54ec5530.BBfZVkUH.css"
     ]
   },
   "virtual:vike:pageConfigLazy:client:/src/pages/artists": {
-    "file": "assets/entries/src_pages_artists.Bp2cdZgh.js",
+    "file": "assets/entries/src_pages_artists.JjYGOtCd.js",
     "name": "entries/src/pages/artists",
     "src": "virtual:vike:pageConfigLazy:client:/src/pages/artists",
     "isEntry": true,
     "isDynamicEntry": true,
     "imports": [
-      "__onRenderClient.vVKCzuIK.js",
-      "_ListPage.BuWjDXyZ.js",
-      "_renderPageClientSide.D8e623kA.js",
-      "_ProfileItem.BNvXYd52.js",
-      "_useFormControl.CvUq2NXJ.js",
-      "_TextField.D97Msb7p.js"
+      "__onRenderClient.CrOezyuq.js",
+      "_ListPage.C3L88Wxa.js",
+      "_renderPageClientSide.DUFOS9QK.js",
+      "_ProfileItem.BmjKx306.js",
+      "_useFormControl.EJpFiZXV.js",
+      "_TextField.DH3ceIwm.js"
     ],
     "css": [
       "assets/static/src_renderer_index-54ec5530.BBfZVkUH.css"
     ]
   },
   "virtual:vike:pageConfigLazy:client:/src/pages/characters": {
-    "file": "assets/entries/src_pages_characters.DMJVXpJ9.js",
+    "file": "assets/entries/src_pages_characters.BjgbtDhl.js",
     "name": "entries/src/pages/characters",
     "src": "virtual:vike:pageConfigLazy:client:/src/pages/characters",
     "isEntry": true,
     "isDynamicEntry": true,
     "imports": [
-      "__onRenderClient.vVKCzuIK.js",
-      "_ListPage.BuWjDXyZ.js",
-      "_renderPageClientSide.D8e623kA.js",
-      "_ProfileItem.BNvXYd52.js",
-      "_useFormControl.CvUq2NXJ.js",
-      "_TextField.D97Msb7p.js"
+      "__onRenderClient.CrOezyuq.js",
+      "_ListPage.C3L88Wxa.js",
+      "_renderPageClientSide.DUFOS9QK.js",
+      "_ProfileItem.BmjKx306.js",
+      "_useFormControl.EJpFiZXV.js",
+      "_TextField.DH3ceIwm.js"
     ],
     "css": [
       "assets/static/src_renderer_index-54ec5530.BBfZVkUH.css"
     ]
   },
   "virtual:vike:pageConfigLazy:client:/src/pages/gallery": {
-    "file": "assets/entries/src_pages_gallery.Bvbqohqi.js",
+    "file": "assets/entries/src_pages_gallery.BFYQgTu0.js",
     "name": "entries/src/pages/gallery",
     "src": "virtual:vike:pageConfigLazy:client:/src/pages/gallery",
     "isEntry": true,
     "isDynamicEntry": true,
     "imports": [
-      "__onRenderClient.vVKCzuIK.js",
-      "_ProfileItem.BNvXYd52.js",
-      "_useFormControl.CvUq2NXJ.js",
-      "_renderPageClientSide.D8e623kA.js"
+      "__onRenderClient.CrOezyuq.js",
+      "_ProfileItem.BmjKx306.js",
+      "_useFormControl.EJpFiZXV.js",
+      "_renderPageClientSide.DUFOS9QK.js"
     ],
     "css": [
       "assets/static/src_renderer_index-54ec5530.BBfZVkUH.css"
     ]
   },
   "virtual:vike:pageConfigLazy:client:/src/pages/index": {
-    "file": "assets/entries/src_pages_index.BlmIkfhl.js",
+    "file": "assets/entries/src_pages_index.BMeU7Ga9.js",
     "name": "entries/src/pages/index",
     "src": "virtual:vike:pageConfigLazy:client:/src/pages/index",
     "isEntry": true,
     "isDynamicEntry": true,
     "imports": [
-      "__onRenderClient.vVKCzuIK.js",
-      "_renderPageClientSide.D8e623kA.js"
+      "__onRenderClient.CrOezyuq.js",
+      "_renderPageClientSide.DUFOS9QK.js"
     ],
     "css": [
       "assets/static/src_renderer_index-54ec5530.BBfZVkUH.css"
     ]
   },
   "virtual:vike:pageConfigLazy:client:/src/pages/posts/@id": {
-    "file": "assets/entries/src_pages_posts_-id.BVoriDk1.js",
+    "file": "assets/entries/src_pages_posts_-id.Bk1T9o5Z.js",
     "name": "entries/src/pages/posts/@id",
     "src": "virtual:vike:pageConfigLazy:client:/src/pages/posts/@id",
     "isEntry": true,
     "isDynamicEntry": true,
     "imports": [
-      "__onRenderClient.vVKCzuIK.js",
-      "_ProfileItem.BNvXYd52.js",
-      "_renderPageClientSide.D8e623kA.js"
+      "__onRenderClient.CrOezyuq.js",
+      "_ProfileItem.BmjKx306.js",
+      "_renderPageClientSide.DUFOS9QK.js"
     ],
     "css": [
       "assets/static/src_renderer_index-54ec5530.BBfZVkUH.css"
