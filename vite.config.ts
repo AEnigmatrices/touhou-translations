429eb233753b4f1bb717c2fa093cf3aa01749968
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-07-28 18:27:42 +0700

    feat: Try vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,26 @@ import generateSitemapPlugin from './plugins/generateSitemapPlugin';
 
 export default defineConfig({
     plugins: [react(), vike(), postDataPlugin, generateSitemapPlugin],
-    base: '/touhou-translations/'
+    base: '/touhou-translations/',
+
+    build: {
+        assetsInlineLimit: 4096,
+
+        cssMinify: true,
+        cssCodeSplit: true,
+
+        minify: 'esbuild',
+        rollupOptions: {
+            output: {
+                chunkFileNames: 'assets/js/[name].[hash].js'
+            }
+        }
+    },
+
+    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.webp'],
+
+    optimizeDeps: {
+        include: ['react', 'react-dom'],
+        exclude: []
+    }
 });
