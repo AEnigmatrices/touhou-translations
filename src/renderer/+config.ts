b316eb8f7809ffdfcee9569d5a8cd6dd52ae18af
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-07-29 08:40:21 +0700

    refactor: Move prerender setting to renderer config

--- src/renderer/+config.ts
+++ src/renderer/+config.ts
@@ -1,4 +1,7 @@
-export default {
+import type { Config } from 'vike/types'
+
+export const config = {
+    prerender: true,
     clientRouting: true,
     hydrationCanBeAborted: true
-}
+} satisfies Config
