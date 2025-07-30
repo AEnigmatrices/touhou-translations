af4ea7460d2b91a9179c7aecfa11f1b51850a828
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-07-30 23:52:55 +0700

    chore: Remove unused hashDateToIndex

--- src/pages/index/utils.ts
+++
@@ -1,7 +0,0 @@
-export const hashDateToIndex = (dateStr: string, max: number): number => {
-    let hash = 0;
-    for (let i = 0; i < dateStr.length; i++) {
-        hash = (hash * 31 + dateStr.charCodeAt(i)) % max;
-    }
-    return Math.abs(hash) % max;
-};
