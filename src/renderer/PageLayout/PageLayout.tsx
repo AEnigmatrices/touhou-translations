0da6f6e2029236f6976922292551fc70ae477123
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-08-04 19:30:37 +0700

    refactor: Switch to Fontsource

--- src/renderer/PageLayout/PageLayout.tsx
+++ src/renderer/PageLayout/PageLayout.tsx
@@ -7,8 +7,27 @@ import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 import styles from "./PageLayout.styles";
 import theme from './theme';
+
 import type { ReactNode } from "react";
 import type { PageContext } from 'vike/types';
+
+import '@fontsource/noto-sans/300.css';
+import '@fontsource/noto-sans/400.css';
+import '@fontsource/noto-sans/600.css';
+import '@fontsource/noto-sans/500.css';
+import '@fontsource/noto-sans/700.css';
+
+import '@fontsource/noto-sans-jp/300.css';
+import '@fontsource/noto-sans-jp/400.css';
+import '@fontsource/noto-sans-jp/500.css';
+import '@fontsource/noto-sans-jp/600.css';
+import '@fontsource/noto-sans-jp/700.css';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/600.css';
+import '@fontsource/roboto/700.css';
 
 
 const PageLayout = ({ pageContext, children }: { pageContext: PageContext; children: ReactNode }) => {
