1c2ab4d735b43280fc845cf7af9d36ec3af33865
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-08-06 08:24:20 +0700

    feat: No need to modify URL for gallery mode

--- src/pages/gallery/useQueryParams.ts
+++ src/pages/gallery/useQueryParams.ts
@@ -8,22 +8,10 @@ const useQueryParams = (urlParsed: Props) => {
     const characterQueries = searchParams.getAll('character');
     const artistQueries = searchParams.getAll('artist');
     const mode: 'and' | 'or' = searchParams.get('mode') === 'or' ? 'or' : 'and';
-    const galleryOnlyParam = searchParams.get('galleryOnly') === 'true';
 
-    const [galleryOnly, setGalleryOnly] = useState(galleryOnlyParam);
+    const [galleryOnly, setGalleryOnly] = useState(false);
 
-    const toggleGalleryOnly = () => {
-        const newGalleryOnly = !galleryOnly;
-        setGalleryOnly(newGalleryOnly);
-
-        const newParams = new URLSearchParams(urlParsed.searchOriginal || '');
-        if (newGalleryOnly) {
-            newParams.set('galleryOnly', 'true');
-        } else {
-            newParams.delete('galleryOnly');
-        }
-        window.history.replaceState(null, '', `${urlParsed.pathname}?${newParams.toString()}`);
-    };
+    const toggleGalleryOnly = () => setGalleryOnly(prev => !prev);
 
     return { characterQueries, artistQueries, mode, galleryOnly, toggleGalleryOnly };
 };
