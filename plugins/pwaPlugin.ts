21bfc6ded576881ead8ad22d5568d47f7e419fec
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-08-07 18:32:04 +0700

    chore: Switch to just 3 icons

--- plugins/pwaPlugin.ts
+++ plugins/pwaPlugin.ts
@@ -14,32 +14,9 @@ export default VitePWA({
         theme_color: "#ffffff",
         description: "An archive of English-translated Touhou Project fan comics and illustrations.",
         "icons": [
-            { "src": "icons/pwa/pwa-icon-48x48.png", "sizes": "48x48", "type": "image/png", "purpose": "any" },
-            { "src": "icons/pwa/pwa-icon-48x48.png", "sizes": "48x48", "type": "image/png", "purpose": "maskable" },
-
-            { "src": "icons/pwa/pwa-icon-72x72.png", "sizes": "72x72", "type": "image/png", "purpose": "any" },
-            { "src": "icons/pwa/pwa-icon-72x72.png", "sizes": "72x72", "type": "image/png", "purpose": "maskable" },
-
-            { "src": "icons/pwa/pwa-icon-96x96.png", "sizes": "96x96", "type": "image/png", "purpose": "any" },
-            { "src": "icons/pwa/pwa-icon-96x96.png", "sizes": "96x96", "type": "image/png", "purpose": "maskable" },
-
-            { "src": "icons/pwa/pwa-icon-128x128.png", "sizes": "128x128", "type": "image/png", "purpose": "any" },
-            { "src": "icons/pwa/pwa-icon-128x128.png", "sizes": "128x128", "type": "image/png", "purpose": "maskable" },
-
-            { "src": "icons/pwa/pwa-icon-144x144.png", "sizes": "144x144", "type": "image/png", "purpose": "any" },
-            { "src": "icons/pwa/pwa-icon-144x144.png", "sizes": "144x144", "type": "image/png", "purpose": "maskable" },
-
-            { "src": "icons/pwa/pwa-icon-152x152.png", "sizes": "152x152", "type": "image/png", "purpose": "any" },
-            { "src": "icons/pwa/pwa-icon-152x152.png", "sizes": "152x152", "type": "image/png", "purpose": "maskable" },
-
-            { "src": "icons/pwa/pwa-icon-192x192.png", "sizes": "192x192", "type": "image/png", "purpose": "any" },
-            { "src": "icons/pwa/pwa-icon-192x192.png", "sizes": "192x192", "type": "image/png", "purpose": "maskable" },
-
-            { "src": "icons/pwa/pwa-icon-384x384.png", "sizes": "384x384", "type": "image/png", "purpose": "any" },
-            { "src": "icons/pwa/pwa-icon-384x384.png", "sizes": "384x384", "type": "image/png", "purpose": "maskable" },
-
-            { "src": "icons/pwa/pwa-icon-512x512.png", "sizes": "512x512", "type": "image/png", "purpose": "any" },
-            { "src": "icons/pwa/pwa-icon-512x512.png", "sizes": "512x512", "type": "image/png", "purpose": "maskable" }
+            { "src": "icons/pwa-icon-48x48.png", "sizes": "48x48", "type": "image/png", "purpose": "any" },
+            { "src": "icons/pwa-icon-192x192.png", "sizes": "192x192", "type": "image/png", "purpose": "maskable" },
+            { "src": "icons/pwa-icon-512x512.png", "sizes": "512x512", "type": "image/png", "purpose": "maskable" }
         ],
         lang: "en"
     },
