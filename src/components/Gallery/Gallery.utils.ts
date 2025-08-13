b544ffbf4d51034c31f16bcf164b871bef630a68
AENigmatrices <alfabravosierra.zero@gmail.com>
2025-08-13 18:27:54 +0700

    refactor: Remove Gallery.utils and use getImageSx instead.

--- src/components/Gallery/Gallery.utils.ts
+++
@@ -1,4 +0,0 @@
-import type { SxProps, Theme } from '@mui/material';
-
-export const mergeSx = (...sxObjects: (SxProps<Theme> | false | undefined)[]): SxProps<Theme> =>
-    Object.assign({}, ...sxObjects.filter(Boolean));
--- src/components/Gallery/GalleryImage.tsx
+++ src/components/Gallery/GalleryImage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Box } from '@mui/material';
-import { mergeSx } from './Gallery.utils';
 import styles from './GalleryImage.styles';
 
 interface Props {
@@ -11,12 +10,15 @@ interface Props {
 const GalleryImage: React.FC<Props> = ({ src, alt }) => {
     const [loaded, setLoaded] = useState(false);
 
-    const sxImage = mergeSx(styles.image, loaded ? styles.loaded : styles.loading);
+    const getImageSx = (loaded: boolean) => ({
+        ...styles.image,
+        ...(loaded ? styles.loaded : styles.loading),
+    });
 
     return (
         <Box sx={styles.wrapper}>
             {!loaded && <Box sx={styles.placeholder} aria-hidden="true" />}
-            <Box component="img" src={src} alt={alt} loading="lazy" onLoad={() => setLoaded(true)} sx={sxImage} />
+            <Box component="img" src={src} alt={alt} loading="lazy" onLoad={() => setLoaded(true)} sx={getImageSx(loaded)} />
         </Box>
     );
 };
