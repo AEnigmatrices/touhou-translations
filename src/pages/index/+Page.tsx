d6558a5dd8657558b73362e92bca00c2f62e1166
AEnigmatrices <alfabravosierra.zero@gmail.com>
2025-09-04 19:29:35 +0700

    style: Styles file for Index page

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -3,21 +3,24 @@ import { Container, Grid, Stack } from '@mui/material';
 import About from './components/About';
 import Video from './components/Video';
 import DailyPost from './components/DailyPost';
+import styles from './styles/Page.styles';
 import type { JSX } from 'react';
 import type { Data } from './+data';
 
 const Page = (): JSX.Element => {
     const { dailyPost } = useData<Data>();
     return (
-        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={{ xs: 3, md: 4 }}>
+        <Container maxWidth="lg" sx={styles.container}>
+            <Grid container spacing={{ xs: 2, md: 4 }}>
                 <Grid size={{ xs: 12, md: 7 }}>
-                    <Stack spacing={2}>
+                    <Stack spacing={{ xs: 1, md: 2 }}>
                         <About />
                         <Video />
                     </Stack>
                 </Grid>
-                <Grid size={{ xs: 12, md: 5 }}><DailyPost dailyPost={dailyPost} /></Grid>
+                <Grid size={{ xs: 12, md: 5 }}>
+                    <DailyPost dailyPost={dailyPost} />
+                </Grid>
             </Grid>
         </Container>
     );
---
+++ src/pages/index/styles/Page.styles.ts
@@ -0,0 +1,14 @@
+import type { SxProps } from '@mui/material/styles';
+
+interface Styles {
+    container: SxProps;
+}
+
+const styles: Styles = {
+    container: {
+        mt: { xs: 2, md: 4 },
+        mb: { xs: 2, md: 4 },
+    }
+};
+
+export default styles;
