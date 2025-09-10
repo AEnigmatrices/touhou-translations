877afe8b925eaee286233f5180d075eeaf9e16f7
AEnigmatrices <alfabravosierra.zero@gmail.com>
2025-09-10 19:19:07 +0700

    refactor: Preload fonts

--- src/pages/+Head.tsx
+++ src/pages/+Head.tsx
@@ -1,38 +1,39 @@
 const isProd = process.env.NODE_ENV === 'production';
+const fontsUrl =
+    "https://fonts.googleapis.com/css2?" +
+    "family=Noto+Sans+JP:wght@400;500;600;700&" +
+    "family=Noto+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&" +
+    "family=Roboto:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap";
 
-const Head = () => {
-    return (
-        <>
-            <meta name="google-site-verification" content="0HESLIqydO91EGI_Nz10xq3aAxhdqBAfCHes6Q6oXrE" />
-            <meta
-                httpEquiv="Content-Security-Policy"
-                content="
-                    default-src 'self';
-                    script-src 'self' 'unsafe-inline';
-                    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-                    font-src 'self' https://fonts.gstatic.com;
-                    img-src 'self' https://i.redd.it data:;
-                    connect-src 'self' https://www.reddit.com https://i.redd.it;
-                    object-src 'none';
-                    base-uri 'self';
-                    frame-src https://www.youtube-nocookie.com;
-                "
-            />
-            <meta name="referrer" content="strict-origin" />
+const Head = () => (
+    <>
+        <meta name="google-site-verification" content="0HESLIqydO91EGI_Nz10xq3aAxhdqBAfCHes6Q6oXrE" />
+        <meta
+            httpEquiv="Content-Security-Policy"
+            content="
+                default-src 'self';
+                script-src 'self' 'unsafe-inline';
+                style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+                font-src 'self' https://fonts.gstatic.com;
+                img-src 'self' https://i.redd.it data:;
+                connect-src 'self' https://www.reddit.com https://i.redd.it;
+                object-src 'none';
+                base-uri 'self';
+                frame-src https://www.youtube-nocookie.com;
+            "
+        />
+        <meta name="referrer" content="strict-origin" />
 
-            {isProd && <link rel="manifest" href="/touhou-translations/manifest.webmanifest" />}
+        {isProd && <link rel="manifest" href="/touhou-translations/manifest.webmanifest" />}
 
-            <link rel="preconnect" href="https://i.redd.it" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-            <link
-                href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;600;700&family=Noto+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Roboto:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
-                rel="stylesheet"
-            />
+        <link rel="preconnect" href="https://i.redd.it" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link rel="preload" as="style" href={fontsUrl} />
+        <link rel="stylesheet" href={fontsUrl} />
 
-            {isProd && <script src="/touhou-translations/registerSW.js"></script>}
-        </>
-    );
-};
+        {isProd && <script src="/touhou-translations/registerSW.js"></script>}
+    </>
+);
 
 export default Head;
